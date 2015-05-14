9821794a6087e9f492a5d5ea25caffcd67968a60
Josh Qberry <joshqberry@gmail.com>
2015-05-13 21:02:25 -0600

    initial29

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,23 +8,7 @@ Rails.application.configure do
   Pusher.key = ENV['key']
   Pusher.secret = ENV['secret']
 
-  begin
-    Rails.application.config.board = Dino::Board.new(Dino::TxRx.new)
-    button = Dino::Components::Button.new(pin: 5, board: Rails.application.config.board)
-
-    button.down do
-      puts 'up'
-      Pusher['button'].trigger!('down', { :some => 'data' })
-    end
-
-    button.up do
-      puts 'down'
-      Pusher['button'].trigger!('up', { :some => 'data' })
-  end
-
-  rescue Dino::BoardNotFound
-    puts 'The board is not connected'
-  end
+  
 
   # Settings specified here will take precedence over those in config/application.rb.
 
